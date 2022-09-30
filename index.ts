6ca7a116f5ab9f8e45f28787cbb9a558e25728fd
Nina Andal Aarvik <nina@sanity.io>
2022-09-30 15:21:49 +0300

    add timerEvent

--- index.ts
+++ index.ts
@@ -1,48 +1,94 @@
-interface TelemetryPayload {
-  userId: string;
-  event: TELEMETRY_EVENT | string;
-  metadata?: Record<string, string>;
-  telemetryDisabled: boolean; //If the user has disabled telemetry, this is true
-}
+// interface TelemetryPayload {
+//   userId: string;
+//   event: TELEMETRY_EVENT | string;
+//   metadata?: Record<string, string>;
+//   telemetryDisabled: boolean; //If the user has disabled telemetry, this is true
+// }
 
-enum TELEMETRY_EVENT {
-  BUTTON_CLICK = "buttonClick",
-}
+// enum TELEMETRY_EVENT {
+//   BUTTON_CLICK = "buttonClick",
+// }
 
 const PROXY_ENDPOINT = "https://www.sanity.io/intake/dp/v1/track";
 
-const getPayload = ({ event, metadata = {}, userId }: TelemetryPayload) =>
-  JSON.stringify({
-    userId: userId,
-    event: "studio-telemetry",
-    properties: {
-      eventAction: event,
-      ...metadata,
-    },
-    context: {
-      library: {
-        name: "http",
-      },
-    },
-    timestamp: new Date().toISOString(),
-  });
+// const getPayload = ({ event, metadata = {}, userId }: TelemetryPayload) =>
+//   JSON.stringify({
+//     userId: userId,
+//     event: "studio-telemetry",
+//     properties: {
+//       eventAction: event,
+//       ...metadata,
+//     },
+//     context: {
+//       library: {
+//         name: "http",
+//       },
+//     },
+//     timestamp: new Date().toISOString(),
+//   });
 
-const telemetryClient = ({
-  event,
-  metadata,
-  userId,
-  telemetryDisabled,
-}: TelemetryPayload) => {
-  //If you set the env variable to 1, you will turn the tracking off. Anything else, and this fetch will run
-  if (!telemetryDisabled) {
+// const telemetryClient = ({
+//   event,
+//   metadata,
+//   userId,
+//   telemetryDisabled,
+// }: TelemetryPayload) => {
+//   //Turn the tracking off
+//   if (!telemetryDisabled) {
+//     fetch(PROXY_ENDPOINT, {
+//       method: "POST", // or 'PUT'
+//       headers: {
+//         authorization: "Basic MXBiSk5jRUpIV09lM0R2ZFB5SXA3WERJQ0o0Og==",
+//       },
+//       body: getPayload({ event, metadata, userId, telemetryDisabled }),
+//     });
+//   }
+// };
+
+var telemetry = function () {
+  this.id = Math.random();
+  this.timer = {
+    start: null,
+    end: null,
+  };
+
+  this.startTimer = function () {
+    this.timer.start = performance.now();
+  };
+
+  this.endTimer = function () {
+    this.timer.end = performance.now();
+    console.log(
+      `Call to doSomething took ${
+        this.timer.end - this.timer.start
+      } milliseconds.`
+    );
+  };
+
+  const time = this.timer.end - this.timer.start;
+
+  if (this.timer.start !== null && this.timer.end !== null) {
+    console.log(time);
     fetch(PROXY_ENDPOINT, {
       method: "POST", // or 'PUT'
       headers: {
         authorization: "Basic MXBiSk5jRUpIV09lM0R2ZFB5SXA3WERJQ0o0Og==",
       },
-      body: getPayload({ event, metadata, userId, telemetryDisabled }),
+      body: JSON.stringify({
+        event: "studio-telemetry",
+        properties: {
+          eventAction: "timer-event",
+          metadata: { time },
+        },
+        context: {
+          library: {
+            name: "http",
+          },
+        },
+        timestamp: new Date().toISOString(),
+      }),
     });
   }
 };
 
-module.exports = telemetryClient;
+module.exports = telemetry;
