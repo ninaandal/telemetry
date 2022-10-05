a8c4d2c93782bc9ff45da32cdb767606beba295d
Rita <rita@sanity.io>
2022-10-05 14:04:05 +0200

    feat: timer works for cli

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const { Console } = require("console");
-
 const PROXY_ENDPOINT = "https://www.sanity.io/intake/dp/v1/track";
 
-const getPayload = ({ event, metadata = {}, userId }) =>
-  JSON.stringify({
+const getPayload = ({ event, metadata = {}, userId }) => {
+  return JSON.stringify({
     userId: userId,
     event: "studio-telemetry",
     properties: {
@@ -17,21 +15,77 @@ const getPayload = ({ event, metadata = {}, userId }) =>
     },
     timestamp: new Date().toISOString(),
   });
-
-const telemetryClient = ({ event, metadata, userId, telemetryDisabled }) => {
-  const fetch = require("cross-fetch");
-
-  //If you set the env variable to 1, you will turn the tracking off. Anything else, and this fetch will run
-  if (!telemetryDisabled) {
-    console.log(fetch);
-    fetch(PROXY_ENDPOINT, {
-      method: "POST", // or 'PUT'
-      headers: {
-        authorization: "Basic MXBiSk5jRUpIV09lM0R2ZFB5SXA3WERJQ0o0Og==",
-      },
-      body: getPayload({ event, metadata, userId, telemetryDisabled }),
-    });
-  }
 };
 
-module.exports = telemetryClient;
+const Telemetry = {
+  telemetryClick: ({ event, metadata, userId, telemetryDisabled }) => {
+    const fetch = require("cross-fetch");
+
+    //If you set the env variable to 1, you will turn the tracking off. Anything else, and this fetch will run
+    if (!telemetryDisabled) {
+      fetch(PROXY_ENDPOINT, {
+        method: "POST", // or 'PUT'
+        headers: {
+          authorization: "Basic MXBiSk5jRUpIV09lM0R2ZFB5SXA3WERJQ0o0Og==",
+        },
+        body: getPayload({ event, metadata, userId, telemetryDisabled }),
+      });
+    }
+  },
+
+  telemetryTimer: ({ event, metadata, userId, telemetryDisabled }) => {
+    const fetch = require("cross-fetch");
+    const id = Math.random();
+    let timerInfo = {
+      start: null,
+      finish: null,
+    };
+
+    //If you set the env variable to 1, you will turn the tracking off. Anything else, and this fetch will run
+    if (!telemetryDisabled) {
+      console.log("timer");
+    }
+
+    start = () => {
+      console.log("start");
+      timerInfo.start = performance.now();
+      const mt = { ...metadata, ...{ start: timerInfo.start, timerId: id } };
+
+      fetch(PROXY_ENDPOINT, {
+        method: "POST", // or 'PUT'
+        headers: {
+          authorization: "Basic MXBiSk5jRUpIV09lM0R2ZFB5SXA3WERJQ0o0Og==",
+        },
+        body: getPayload({
+          event,
+          metadata: mt,
+          userId,
+        }),
+      });
+    };
+    end = () => {
+      console.log("end");
+      timerInfo.end = performance.now();
+      const mt = {
+        ...metadata,
+        ...{ timeSpent: timerInfo.end - timerInfo.start, timerId: id },
+      };
+
+      fetch(PROXY_ENDPOINT, {
+        method: "POST", // or 'PUT'
+        headers: {
+          authorization: "Basic MXBiSk5jRUpIV09lM0R2ZFB5SXA3WERJQ0o0Og==",
+        },
+        body: getPayload({
+          event,
+          metadata: mt,
+          userId,
+        }),
+      });
+    };
+
+    return { start, end };
+  },
+};
+
+module.exports = Telemetry;
