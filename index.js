c31341aa685dd5ccf5212985d400a2f69a29b8b2
Rita <rita@sanity.io>
2022-10-06 10:27:01 +0200

    feat: use code from add-timer-cli but to work on studio

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import fetch from "cross-fetch";
 const PROXY_ENDPOINT = "https://www.sanity.io/intake/dp/v1/track";
 
 const getPayload = ({ event, metadata = {}, userId }) => {
@@ -17,75 +18,82 @@ const getPayload = ({ event, metadata = {}, userId }) => {
   });
 };
 
-const Telemetry = {
-  telemetryClick: ({ event, metadata, userId, telemetryDisabled }) => {
-    const fetch = require("cross-fetch");
+export const telemetryClick = ({
+  event,
+  metadata,
+  userId,
+  telemetryDisabled,
+}) => {
+  console.log("click");
+  //const fetch = require("cross-fetch");
 
-    //If you set the env variable to 1, you will turn the tracking off. Anything else, and this fetch will run
-    if (!telemetryDisabled) {
-      fetch(PROXY_ENDPOINT, {
-        method: "POST", // or 'PUT'
-        headers: {
-          authorization: "Basic MXBiSk5jRUpIV09lM0R2ZFB5SXA3WERJQ0o0Og==",
-        },
-        body: getPayload({ event, metadata, userId, telemetryDisabled }),
-      });
-    }
-  },
-
-  telemetryTimer: ({ event, metadata, userId, telemetryDisabled }) => {
-    const fetch = require("cross-fetch");
-    const id = Math.random();
-    let timerInfo = {
-      start: null,
-      finish: null,
-    };
-
-    //If you set the env variable to 1, you will turn the tracking off. Anything else, and this fetch will run
-    if (!telemetryDisabled) {
-      console.log("timer");
-    }
-
-    start = () => {
-      console.log("start");
-      timerInfo.start = performance.now();
-      const mt = { ...metadata, ...{ start: timerInfo.start, timerId: id } };
-
-      fetch(PROXY_ENDPOINT, {
-        method: "POST", // or 'PUT'
-        headers: {
-          authorization: "Basic MXBiSk5jRUpIV09lM0R2ZFB5SXA3WERJQ0o0Og==",
-        },
-        body: getPayload({
-          event,
-          metadata: mt,
-          userId,
-        }),
-      });
-    };
-    end = () => {
-      console.log("end");
-      timerInfo.end = performance.now();
-      const mt = {
-        ...metadata,
-        ...{ timeSpent: timerInfo.end - timerInfo.start, timerId: id },
-      };
-
-      fetch(PROXY_ENDPOINT, {
-        method: "POST", // or 'PUT'
-        headers: {
-          authorization: "Basic MXBiSk5jRUpIV09lM0R2ZFB5SXA3WERJQ0o0Og==",
-        },
-        body: getPayload({
-          event,
-          metadata: mt,
-          userId,
-        }),
-      });
-    };
-
-    return { start, end };
-  },
+  //If you set the env variable to 1, you will turn the tracking off. Anything else, and this fetch will run
+  if (!telemetryDisabled) {
+    fetch(PROXY_ENDPOINT, {
+      method: "POST", // or 'PUT'
+      headers: {
+        authorization: "Basic MXBiSk5jRUpIV09lM0R2ZFB5SXA3WERJQ0o0Og==",
+      },
+      body: getPayload({ event, metadata, userId, telemetryDisabled }),
+    });
+  }
 };
 
-module.exports = Telemetry;
+export const telemetryTimer = ({
+  event,
+  metadata,
+  userId,
+  telemetryDisabled,
+}) => {
+  //const fetch = require("cross-fetch");
+  const id = Math.random();
+  let timerInfo = {
+    start: null,
+    finish: null,
+  };
+
+  //If you set the env variable to 1, you will turn the tracking off. Anything else, and this fetch will run
+  if (!telemetryDisabled) {
+    console.log("timer");
+  }
+
+  const start = () => {
+    console.log("start");
+    timerInfo.start = performance.now();
+    const mt = { ...metadata, ...{ start: timerInfo.start, timerId: id } };
+
+    fetch(PROXY_ENDPOINT, {
+      method: "POST", // or 'PUT'
+      headers: {
+        authorization: "Basic MXBiSk5jRUpIV09lM0R2ZFB5SXA3WERJQ0o0Og==",
+      },
+      body: getPayload({
+        event,
+        metadata: mt,
+        userId,
+      }),
+    });
+  };
+  const end = () => {
+    console.log("end");
+    timerInfo.end = performance.now();
+    const mt = {
+      ...metadata,
+      ...{ timeSpent: timerInfo.end - timerInfo.start, timerId: id },
+    };
+
+    fetch(PROXY_ENDPOINT, {
+      method: "POST", // or 'PUT'
+      headers: {
+        authorization: "Basic MXBiSk5jRUpIV09lM0R2ZFB5SXA3WERJQ0o0Og==",
+      },
+      body: getPayload({
+        event,
+        metadata: mt,
+        userId,
+      }),
+    });
+  };
+
+  return { start, end };
+};
